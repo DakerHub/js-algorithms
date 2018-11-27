c8a458a95d3de95b14d8d432b967e57def2fe881
chenchengfeng <523246656@qq.com>
2018-11-27 23:59:22 +0800

    [update]:晚äæ完善tree的功能

--- src/lib/BinarySearchTree.ts
+++ src/lib/BinarySearchTree.ts
@@ -7,9 +7,6 @@ interface Node {
 export default class BinarySearchTree {
   root: Node | null = null
   constructor() {}
-  getRoot(): Node | null {
-    return this.root
-  }
   insert(key: any): void {
     const newNode = {
       key,
@@ -40,5 +37,72 @@ export default class BinarySearchTree {
         }
       }
     }
+  }
+  search(key: any): boolean {
+    let currentNode = this.root
+
+    while (currentNode) {
+      if (key < currentNode.key) {
+        currentNode = currentNode.left
+      } else if (key > currentNode.key) {
+        currentNode = currentNode.right
+      } else {
+        return true
+      }
+    }
+
+    return false
+  }
+  inOrderTraverse(callback: Function): void {
+    this.inOrderTraverseNode(callback, this.root)
+  }
+  preOrderTraverse(callback: Function): void {
+    this.preOrderTraverseNode(callback, this.root)
+  }
+  postOrderTraverse(callback: Function): void {
+    this.postOrderTraverseNode(callback, this.root)
+  }
+  min(): any {
+    let currentNode = this.root
+    while (currentNode) {
+      if (currentNode.left) {
+        currentNode = currentNode.left
+      } else {
+        return currentNode.key
+      }
+    }
+  }
+  max(): any {
+    let currentNode = this.root
+    while (currentNode) {
+      if (currentNode.right) {
+        currentNode = currentNode.right
+      } else {
+        return currentNode.key
+      }
+    }
+  }
+  private inOrderTraverseNode(callback: Function, node: Node | null): void {
+    if (!node) return
+
+    this.inOrderTraverseNode(callback, node.left)
+
+    callback(node)
+
+    this.inOrderTraverseNode(callback, node.right)
+  }
+  private preOrderTraverseNode(callback: Function, node: Node | null): void {
+    if (!node) return
+
+    callback(node)
+    this.preOrderTraverseNode(callback, node.left)
+    this.preOrderTraverseNode(callback, node.right)
+  }
+  private postOrderTraverseNode(callback: Function, node: Node | null): void {
+    if (!node) return
+
+    this.postOrderTraverseNode(callback, node.left)
+    this.postOrderTraverseNode(callback, node.right)
+    callback(node)
   }
 }
--- test/BinarySearchTree.test.ts
+++ test/BinarySearchTree.test.ts
@@ -4,7 +4,7 @@ describe('测试 二叉搜索树', () => {
   describe('初始化', () => {
     it('root 应该为 null', () => {
       const tree = new BinarySearchTree()
-      const root = tree.getRoot()
+      const root = tree.root
       expect(root).toBeNull()
     })
   })
@@ -14,11 +14,97 @@ describe('测试 二叉搜索树', () => {
       tree.insert(2)
       tree.insert(5)
       tree.insert(1)
-      const root = tree.getRoot()
+      const root = tree.root
       expect(root).not.toBeNull()
       expect(root.key).toBe(2)
       expect(root.left.key).toBe(1)
       expect(root.right.key).toBe(5)
+    })
+  })
+
+  describe('测试 search', () => {
+    const tree = new BinarySearchTree()
+    tree.insert(2)
+    tree.insert(5)
+    tree.insert(1)
+    it('依次插入2、5、1，查找1，应返回true', () => {
+      expect(tree.search(1)).toBe(true)
+    })
+    it('依次插入2、5、1，查找1，应返回true', () => {
+      expect(tree.search(2)).toBe(true)
+    })
+    it('依次插入2、5、1，查找0，应返回false', () => {
+      expect(tree.search(0)).toBe(false)
+    })
+  })
+
+  describe('测试 遍历', () => {
+    let tree = new BinarySearchTree()
+    it('依次插入6、5、1、7、8、10、2', () => {
+      tree.insert(6)
+      tree.insert(5)
+      tree.insert(1)
+      tree.insert(7)
+      tree.insert(8)
+      tree.insert(10)
+      tree.insert(2)
+      const root = tree.root
+      expect(root.key).toBe(6)
+      expect(root.left.key).toBe(5)
+      expect(root.right.key).toBe(7)
+    })
+    it('中序遍历 顺序为1, 2, 5, 6, 7, 8, 10', () => {
+      const result = []
+      const callback = node => {
+        result.push(node.key)
+      }
+
+      tree.inOrderTraverse(callback)
+
+      expect(result).toEqual([1, 2, 5, 6, 7, 8, 10])
+    })
+    it('先序遍历 顺序为6, 5, 1, 2, 7, 8, 10', () => {
+      const result = []
+      const callback = node => {
+        result.push(node.key)
+      }
+
+      tree.preOrderTraverse(callback)
+
+      expect(result).toEqual([6, 5, 1, 2, 7, 8, 10])
+    })
+    it('后序遍历 顺序为2, 1, 5, 10, 8, 7, 6', () => {
+      const result = []
+      const callback = node => {
+        result.push(node.key)
+      }
+
+      tree.postOrderTraverse(callback)
+
+      expect(result).toEqual([2, 1, 5, 10, 8, 7, 6])
+    })
+  })
+
+  describe('测试 最值', () => {
+    let tree = new BinarySearchTree()
+    it('依次插入6、5、1、7、8、10、2', () => {
+      tree.insert(6)
+      tree.insert(5)
+      tree.insert(1)
+      tree.insert(7)
+      tree.insert(8)
+      tree.insert(10)
+      tree.insert(2)
+      const root = tree.root
+      expect(root.key).toBe(6)
+      expect(root.left.key).toBe(5)
+      expect(root.right.key).toBe(7)
+    })
+    it('调用min 返回1', () => {
+      expect(tree.min()).toBe(1)
+    })
+    it('调用max 返回10', () => {
+      expect(tree.max()).toBe(10)
     })
   })
 })
